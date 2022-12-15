876d2a8d414ab00c4d1fdc03b8dee288ed3e4be4
Nissekissen <nils.m.lindblad@gmail.com>
2022-12-15 22:50:39 +0100

    A bit of auto formatting ugh

--- src/events/ready.js
+++ src/events/ready.js
@@ -1,12 +1,19 @@
-const { ActivityType } = require('discord.js')
-const Tags = require('../index.js')
-const logger = require('../utils/logger.js')
+const { ActivityType } = require("discord.js");
+const Tags = require("../index.js");
+const logger = require("../utils/logger.js");
 
 module.exports = {
-    name: 'ready',
+    name: "ready",
     once: true,
     execute(client) {
-        logger.log('Bot is ready!')
-        client.user.setPresence({ activities: [{ name: 'Bible: Audio Book' , type: ActivityType.Listening}], status: 'online' })
-    }
-}
+        logger.log("Bot is ready!");
+        client.user.setPresence({
+            activities: [
+                { name: "Bible: Audio Book", type: ActivityType.Listening },
+            ],
+            status: "online",
+        });
+
+        
+    },
+};
